a1ad582cfe959cffdfe48351f547b5884662daa6
MiraySav <100086719+MiraySav@users.noreply.github.com>
2022-08-30 22:52:42 +0200

    Initial commit

---
+++ Register Test Suite/REG01.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote import switch_to
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+
+
+serv_obj = Service("C:\WebDrivers\chromedriver_win32\chromedriver.exe")
+driver = webdriver.Chrome(service=serv_obj)
+driver.get("https://www.amazon.com.tr/")
+driver.maximize_window()
+driver.implicitly_wait(5)
+action = ActionChains(driver)
+
+# Accept Cookies
+driver.find_element(By.XPATH, "//input[@data-cel-widget='sp-cc-accept']").click()
+action.move_to_element(driver.find_element(By.XPATH, "//span[contains(normalize-space(),'Hesap ve Listeler')]"))
+action.perform()
+driver.find_element(By.XPATH, "//div[@id='nav-flyout-ya-newCust']//a[@class='nav-a'][normalize-space()='Üye olun.']").click()
+driver.find_element(By.XPATH, "//input[@id='ap_customer_name']").send_keys("Miray Sav")
+driver.find_element(By.XPATH, "//input[@id='ap_email']").send_keys("miraytest123@gmail.com")
+driver.find_element(By.XPATH, "//input[@id='ap_password']").send_keys("Test123.")
+driver.find_element(By.XPATH, "//input[@id='ap_password_check']").send_keys("Test123.")
+driver.find_element(By.XPATH, "//input[@name='legalMarketingCheckBox']").click()
+driver.find_element(By.XPATH, "//input[@aria-labelledby='auth-continue-announce']").click()
+
+driver.switch_to.frame(driver.find_element(By.ID, "CaptchaFrame"))
+driver.find_element(By.XPATH, "This element is in iframe - //button[@id='home_children_button']").click()
+#Tüm bilgiler doğru olduğunda giriş tuşu çalışıyor
---
+++ Register Test Suite/REG02.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+
+
+serv_obj = Service("C:\WebDrivers\chromedriver_win32\chromedriver.exe")
+driver = webdriver.Chrome(service=serv_obj)
+driver.get("https://www.amazon.com.tr/")
+driver.maximize_window()
+driver.implicitly_wait(5)
+action = ActionChains(driver)
+
+# Accept Cookies
+driver.find_element(By.XPATH, "//input[@data-cel-widget='sp-cc-accept']").click()
+action.move_to_element(driver.find_element(By.XPATH, "//span[contains(normalize-space(),'Hesap ve Listeler')]"))
+action.perform()
+driver.find_element(By.XPATH, "//div[@id='nav-flyout-ya-newCust']//a[@class='nav-a'][normalize-space()='Üye olun.']").click()
+driver.find_element(By.XPATH, "//input[@id='ap_customer_name']").send_keys("Miray Sav")
+driver.find_element(By.XPATH, "//input[@id='ap_email']").send_keys("miraytest123")
+driver.find_element(By.XPATH, "//input[@id='ap_password']").send_keys("Test123.")
+driver.find_element(By.XPATH, "//input[@id='ap_password_check']").send_keys("Test123.")
+driver.find_element(By.XPATH, "//input[@name='legalMarketingCheckBox']").click()
+driver.find_element(By.XPATH, "//input[@aria-labelledby='auth-continue-announce']").click()
+
+#Yanlış e posta adresi uyarısı veriyor
